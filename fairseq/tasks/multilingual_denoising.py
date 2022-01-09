3b3acb83ec62c7e215b4cc20bce3b98306ff9721
sarubi <stsarut@gmail.com>
2022-01-09 21:48:48 +0530

    multilinugal denosing for selected pairs

--- fairseq/tasks/multilingual_denoising.py
+++ fairseq/tasks/multilingual_denoising.py
@@ -70,8 +70,11 @@ class MultilingualDenoisingTask(DenoisingTask):
             languages = args.langs.split(",")
 
         if args.add_lang_token:
-            for lang in languages:
+            langs_load = "ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN,af_ZA,az_AZ,bn_IN,fa_IR,he_IL,hr_HR,id_ID,ka_GE,km_KH,mk_MK,ml_IN,mn_MN,mr_IN,pl_PL,ps_AF,pt_XX,sv_SE,sw_KE,ta_IN,te_IN,th_TH,tl_XX,uk_UA,ur_PK,xh_ZA,gl_ES,sl_SI"
+            langs_to_add = langs_load.split(",")
+            for lang in langs_to_add:
                 dictionary.add_symbol("[{}]".format(lang))
+
 
         logger.info("dictionary: {} types".format(len(dictionary)))
         if not hasattr(args, "shuffle_instance"):
